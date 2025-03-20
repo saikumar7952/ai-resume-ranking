6b8aa6668916fb8df890e15bc4d0e79d8313eba7
saikumar7952 <138636833+saikumar7952@users.noreply.github.com>
2025-03-20 16:27:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from PyPDF2 import PdfReader # Imports the PdfReader class from the PyPDF2 library.
+from PyPDF2 import PdfReader 
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -8,21 +8,17 @@ def extract_text_from_pdf(file):
     text = ""
     for page in pdf.pages:
         text += page.extract_text()
-    return text # Return the extracted text
+    return text 
 
 
 
 def rank_resumes(job_description, resumes):
-    # Combine job description with resumes
     documents = [job_description] + resumes
     vectorizer = TfidfVectorizer().fit_transform(documents)
     vectors = vectorizer.toarray()
-
-    # Calculate cosine similarity
     job_description_vector = vectors[0]
     resume_vectors = vectors[1:]
     cosine_similarities = cosine_similarity([job_description_vector], resume_vectors).flatten()
-
     return cosine_similarities
 st.title("AI Resume Screening & Candidate Ranking System")
 st.header("Job Description")
@@ -36,11 +32,7 @@ if uploaded_files and job_description:
     for file in uploaded_files:
         text = extract_text_from_pdf(file)
         resumes.append(text)
-
-    # Rank resumes
     scores = rank_resumes(job_description, resumes)
-
-    # Display scores
     results = pd.DataFrame({"Resume": [file.name for file in uploaded_files], "Score": scores })
     results = results.sort_values(by="Score", ascending=False)
 
